2769d206b4d62c681ef927969cebfb34eb785323
AppVeyor <ci@appveyor.com>
2023-01-25 18:54:51 +0700

    done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let arr = [];
+  let exprArr = expr.split("");
+
+  while(exprArr.length) {
+    arr.push(exprArr.splice(-10));
+  }
+
+  arr.reverse();
+
+  nextArr:
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = 0; j < 10; j++) {
+      if(arr[i][0] === '0') {
+        arr[i].splice(0, 1);
+      } else {
+        continue nextArr;
+      }
+    } 
+  }
+
+  let tens = /10/g;
+  let eleven = /11/g;
+  let stars = /\*{10}/g;
+  
+  arr = arr.map(item => item.join("")).map((item) => item.replace(eleven, '-').replace(tens, '.').replace(stars, ' '));
+  
+  let finalArr = [];
+  
+  nextStr:
+  for(let i = 0; i < arr.length; i++) {
+    for(let key in MORSE_TABLE) {
+      if (arr[i] === key) {
+        finalArr.push(MORSE_TABLE[key]);
+        continue nextStr;
+      } else if (arr[i] == " ") {
+        finalArr.push(" ");
+        continue nextStr;
+      }
+    }
+  }
+  
+  return finalArr.join("");
 }
 
 module.exports = {
